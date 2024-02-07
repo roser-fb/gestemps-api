0b34e2913a1209c7d558e906730f82fe9938da3b
Roser Fuster Bertomeu <rfusterb@uoc.edu>
2024-02-07 13:25:48 +0100

    server-netlify versio5

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const router = express.Router();
 const serverless = require('serverless-http')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const connect = require('../src/db.js')
+const connect = require('./src/db.js');
 connect();
 require('dotenv').config();
 
@@ -11,12 +12,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.get('/.netlify/functions/api', (_, res) => res.send('Hello World!'));
 
-app.use('/api/user', require('./routes/user'));
+app.use('/user', require('./routes/user'));
 
-app.use('/api/periodes', require('./routes/vacances'));
-app.use('/api/festius', require('./routes/festius'));
-app.use('/api/calendari', require('./routes/calendari'));
-app.use('/api/guardia', require('./routes/guardia'));
-app.use('/api/fitxar', require('./routes/fitxar'));
+app.use('/periodes', require('./routes/vacances'));
+app.use('/festius', require('./routes/festius'));
+app.use('/calendari', require('./routes/calendari'));
+app.use('/guardia', require('./routes/guardia'));
+app.use('/fitxar', require('./routes/fitxar'));
 
+app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app)
